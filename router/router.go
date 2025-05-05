8306456a008eae494e099b7ddbcc2532e2f8936a
sayasurvey <sayasurvey@yahoo.co.jp>
2025-05-05 22:29:44 +0900

    ADD: 本の登録と一覧取得機能(作成途中)

--- router/router.go
+++ router/router.go
@@ -9,6 +9,11 @@ func GetRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/", controller.SayHello)
+	api := r.Group("/api")
+	{
+		api.GET("/books", controller.GetBooks)
+		api.POST("/books", controller.CreateBook)
+	}
 
 	return r
 }
